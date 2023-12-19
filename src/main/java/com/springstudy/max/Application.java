5010090074dcf2fa75948722414048c2f7cc11ed
ZeroKgun <[Github>
2023-12-20 00:10:31 +0900

    2장 HelloControllerTest 까지

---
+++ src/main/java/com/springstudy/max/Application.java
@@ -0,0 +1,11 @@
+package com.springstudy.max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
