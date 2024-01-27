5ac3fb9776726d0c3c2cf6ced8063c19a5742540
ZeroKgun <[Github>
2024-01-27 22:16:11 +0900

    feat.google 로그인 까지

---
+++ src/main/java/com/springstudy/max/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.springstudy.max.config;
+
+
+import com.springstudy.max.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/springstudy/max/web/IndexController.java
+++ src/main/java/com/springstudy/max/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.springstudy.max.web;
 
+import com.springstudy.max.config.auth.LoginUser;
+import com.springstudy.max.config.auth.dto.SessionUser;
 import com.springstudy.max.service.posts.PostsService;
 import com.springstudy.max.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",
                 postsService.findAllDescs());
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
